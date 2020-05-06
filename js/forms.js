94f172b299372dbb5d1708d0ae05bdedf3495925
emmanuel abraham <sundayemmanuelabraham@gmail.com>
2020-05-06 10:08:25 +0100

    adde linkedin image

--- js/forms.js
+++ js/forms.js
@@ -16,6 +16,15 @@ $('#enterprise-brochure').submit(function(e) {
   });
 });
 
+$('#enterprise-demo').submit(function (e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr('action'), $form.serialize()).then(function () {
+    window.location = '../thanks.html/';
+  });
+});
+
 $('#quick-book-brochure').submit(function(e) {
   e.preventDefault();
 
